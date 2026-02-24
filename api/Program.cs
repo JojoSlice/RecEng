3e1394fb73f0a49198414c0aa10cd06484926736
JojoSlice <johannes152@hotmail.com>
2026-02-24 23:08:58 +0100

    clean up in program.cs

--- api/Program.cs
+++ api/Program.cs
@@ -3,16 +3,13 @@ using api.Features.Users;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddOpenApi();
 builder.Services.AddDbContext<AppDbContext>(options =>
-      options.UseNpgsql(builder.Configuration.GetConnectionString("Default")));
+    options.UseNpgsql(builder.Configuration.GetConnectionString("Default"))
+);
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+if (app.Environment.IsDevelopment()) { }
 
 app.UseHttpsRedirection();
 
